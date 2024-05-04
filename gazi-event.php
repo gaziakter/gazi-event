3d19d2b4430d653cc43fa666f8d2f99173e04387
Gazi Akter <gaziakterdev@gmail.com>
2024-05-04 11:00:18 +0600

    custom post type and meta box

--- gazi-event.php
+++ gazi-event.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name:       Gazi Event
  * Plugin URI:        https://gaziakter.com/plugins/gazi-event/
- * Description:       Handle the basics with this plugin.
+ * Description:       Handle the basics event management with this plugin.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
@@ -17,27 +17,77 @@
  if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
-
 class Gazi_Event {
 
     // Constructor
     public function __construct() {
-        // Initialize plugin
-        add_action('init', array($this, 'init'));
-
         // Load text domain for translation
         add_action('plugins_loaded', array($this, 'load_textdomain'));
-    }
 
-    // Initialize plugin
-    public function init() {
-        // Add hooks and filters here
-        
+        //Custom post type for event
+        add_action('init', array($this, 'events_custom_post_type'));
+
+        // Event metabox
+        add_action('add_meta_boxes', array($this, 'add_event_date_meta_box'));
+
+        // Save meta data
+        add_action('save_post', array($this, 'save_event_date_meta_box'));
     }
 
     // Load text domain for translation
     public function load_textdomain() {
-        load_plugin_textdomain('plugin-template', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+        load_plugin_textdomain('gazi-event', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+    }
+
+    //Event custom post type
+    public function events_custom_post_type(){
+        
+        register_post_type('events', array(
+            'labels' => array(
+                'name' => __('Events', 'gazi-event'),
+                'singular_name' => __('Event', 'gazi-event'),
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'supports' => array( 'title', 'editor', 'thumbnail' ),
+        ));
+    }
+
+    public function add_event_date_meta_box(){
+        add_meta_box(
+            'event-date-meta-box',
+            __('Event Date', 'gazi-event'),
+            array($this, 'render_event_date_meta_box'),
+            'events',
+            'normal',
+            'default'
+        );
+    }
+
+    // Retrieve the current value of the 'data' meta field
+    public function render_event_date_meta_box($post) {
+        // Retrieve the current value of the 'data' meta field
+        $event_date = get_post_meta($post->ID, 'event_date', true);
+
+        wp_nonce_field( 'save_event_date', 'event_date_nonce' );
+        // Render HTML for event date input field
+        ?>
+        <label for="event-date"><?php _e('Event Date:', 'gazi-event'); ?></label>
+        <input type="date" id="event-date" name="event_date" value="<?php echo esc_attr($event_date); ?>">
+        <?php
+    }
+
+    // Save meta data
+    public function save_event_date_meta_box($post_id){
+
+        // Check if nonce is set
+        if ( !isset($_POST['event_date_nonce']) || !wp_verify_nonce( $_POST['event_date_nonce'], 'save_event_date' ) ) {
+            return;
+        }
+        
+        if (isset($_POST['event_date'])) {
+            update_post_meta($post_id, 'event_date', sanitize_text_field($_POST['event_date']));
+        }
     }
 
 }
